41b810d5e841d2ba9e75e17cfb8dff68189817f1
semen-good-mood <semenpostnikov093@gmail.com>
2025-01-28 00:37:08 +0300

    auth with yandex

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    'nuxt-auth-utils',
+    '@prisma/nuxt',
+
+  ],
+  runtimeConfig: {
+    oauth: {
+      // provider in lowercase (github, google, etc.)
+      yandex: {
+        clientId: 'a53de23a5cf748f2a95027c42892f88b',
+        clientSecret: 'ff81865a37e44f8bbaa9bfbdedd02d48'
+      }
+    }
+  }
+
 })
