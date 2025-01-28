ad4fbabad341f0f401078581aee370d49523c2e4
semen-good-mood <semenpostnikov093@gmail.com>
2025-01-28 21:13:50 +0300

    Revert "auth with yandex"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    'nuxt-auth-utils',
-    '@prisma/nuxt',
-
-  ],
-  runtimeConfig: {
-    oauth: {
-      // provider in lowercase (github, google, etc.)
-      yandex: {
-        clientId: process.env.YANDEX_CLIENT_ID,
-        clientSecret:  process.env.YANDEX_CLIENT_SECRET
-      }
-    }
-  }
-
+  devtools: { enabled: true }
 })
