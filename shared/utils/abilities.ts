c65ed5de0c2a5a318ca1eff4d85cee78d0cb8440
semen-good-mood <semenpostnikov093@gmail.com>
2025-01-30 01:44:21 +0300

    auth_rolse

---
+++ shared/utils/abilities.ts
@@ -0,0 +1,22 @@
+import type { User } from "@prisma/client"
+
+
+export const guest = defineAbility({ allowGuest: true }, (user: User | null) => true) //гостевой режим
+
+
+export const authsUsert = defineAbility(() => true) 
+
+
+export const createrRoutes = defineAbility((user:User , post: Post) => {
+  return user.yandexId === post.authorId
+})
+
+
+export const adminUser = defineAbility((user:User) => {
+    return user.is_admin
+})    
+
+// нужна типизация
+
+//
+
