dbe3cb2744671510a85f80a42141281246b7ac7c
semen-good-mood <semenpostnikov093@gmail.com>
2025-02-05 16:43:31 +0300

    test1

---
+++ prisma/migrations/20250205132924_full/migration.sql
@@ -0,0 +1,152 @@
+-- CreateTable
+CREATE TABLE "UserAdmin" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    CONSTRAINT "UserAdmin_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Route" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "creater_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "descripsion" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME,
+    "private" BOOLEAN NOT NULL,
+    "approved" BOOLEAN,
+    CONSTRAINT "Route_creater_id_fkey" FOREIGN KEY ("creater_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "RoultePlace" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "description" TEXT,
+    "routed_id" INTEGER NOT NULL,
+    "lat" REAL NOT NULL,
+    "lot" REAL NOT NULL,
+    CONSTRAINT "RoultePlace_routed_id_fkey" FOREIGN KEY ("routed_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "RoutePlaceImage" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "image_id" INTEGER NOT NULL,
+    "route_place_id" INTEGER NOT NULL,
+    CONSTRAINT "RoutePlaceImage_route_place_id_fkey" FOREIGN KEY ("route_place_id") REFERENCES "RoultePlace" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "RoutePlaceImage_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "Image" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "alt" TEXT,
+    "src" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "RouteImage" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "route_id" INTEGER NOT NULL,
+    "image_id" INTEGER NOT NULL,
+    CONSTRAINT "RouteImage_route_id_fkey" FOREIGN KEY ("route_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "RouteImage_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "Image" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Moder" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "route_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    CONSTRAINT "Moder_route_id_fkey" FOREIGN KEY ("route_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Moder_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Coments" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "route_id" INTEGER NOT NULL,
+    "text" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL,
+    CONSTRAINT "Coments_route_id_fkey" FOREIGN KEY ("route_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Coments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Rating" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "route_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "value" INTEGER NOT NULL,
+    CONSTRAINT "Rating_route_id_fkey" FOREIGN KEY ("route_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Rating_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Visited" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "route_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "visited_at" DATETIME NOT NULL,
+    CONSTRAINT "Visited_route_id_fkey" FOREIGN KEY ("route_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Visited_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "City" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "route_id" INTEGER NOT NULL,
+    CONSTRAINT "City_route_id_fkey" FOREIGN KEY ("route_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "AuditLog" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "table_name" TEXT NOT NULL,
+    "record_id" INTEGER NOT NULL,
+    "creater_id" INTEGER NOT NULL,
+    "operation" TEXT NOT NULL,
+    "old_data" JSONB,
+    "new_data" JSONB,
+    "changed_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "AuditLog_creater_id_fkey" FOREIGN KEY ("creater_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "AuditLog_record_id_fkey" FOREIGN KEY ("record_id") REFERENCES "Route" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "AuditLog_record_id_fkey" FOREIGN KEY ("record_id") REFERENCES "Image" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "AuditLog_record_id_fkey" FOREIGN KEY ("record_id") REFERENCES "RoultePlace" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("id", "name") SELECT "id", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserAdmin_user_id_key" ON "UserAdmin"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RoutePlaceImage_image_id_key" ON "RoutePlaceImage"("image_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RouteImage_image_id_key" ON "RouteImage"("image_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Visited_route_id_key" ON "Visited"("route_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Visited_user_id_key" ON "Visited"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "City_route_id_key" ON "City"("route_id");
--- server/api/routes/create.post.ts
+++ server/api/routes/create.post.ts
@@ -1,6 +1,59 @@
 import prisma from "~/lib/prisma";
 
 
-export default eventHandler(async(event)=>{
-    const {name, description,id,user_id,} = await readBody(event)
-})
+export default eventHandler(async(event)=>{ 
+
+        const { creater_id, name, descripsion, privateRoute, approved, places, route_images, route_place_images  } = await readBody(event)
+    
+
+        const newRoute = await prisma.route.create({
+          data: {
+            creater_id,
+            name,
+            descripsion,
+            private: privateRoute || false,
+            approved: approved || false,
+            roulte_place: {
+              create: places.map((place) => ({
+                name: place.name,
+                description: place.description,
+                lat: place.lat,
+                lot: place.lot,
+                route_place_images:{
+                    image: {
+                        create:route_place_images.map((image) => ({
+                            alt:image.alt,
+                            src:image.src
+                        }))
+                    
+                    }
+                }, include: {
+                    route_place_images: true
+                }
+
+              })),
+             
+            },
+            route_image: {
+                image: {
+                    create: route_images.map((image) => ({
+                        alt:image.alt,
+                        src:image.src
+                    }))
+                }, 
+                include: {
+                    image: true
+                }
+            },
+           
+          },
+          include: {
+            roultePlace: true,
+            route_image: true,
+            
+          },
+        });
+    });
+    
+
+
