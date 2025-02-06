c322d2b393bd8bda8a36324e1fbee2ae40fdbe91
semen-good-mood <semenpostnikov093@gmail.com>
2025-02-06 13:41:32 +0300

    delete

---
+++ server/api/moder/delete.delete.ts
@@ -0,0 +1,29 @@
+import prisma from "~/lib/prisma"
+import { user } from "~/use.vue"
+
+interface Body{
+    route_id: number
+}
+export default eventHandler(async(event) => {
+    const {route_id} = await readBody<Body>(event)
+
+
+    const id = await prisma.moder.findFirst({
+        where:{
+            route_id,
+            user_id:user.yandexId
+        },
+        select:{
+            id:true
+        }
+    })
+    if (id) {
+        await prisma.moder.delete({
+            where:{
+                id:+id
+            }
+        }) 
+    }
+   
+
+})
