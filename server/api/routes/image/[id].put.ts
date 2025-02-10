6ad9a45c4676f808ec098eb3a223c48ef30692dd
derloky <semenpostnikov093@gmail.com>
2025-02-10 20:27:57 +0300

    ю

--- server/api/routes/image/[id].put.ts
+++ server/api/routes/image/[id].put.ts
@@ -1,4 +1,6 @@
+import { useCurrentUser } from "~/composable/useCurrentUser"
 import prisma from "~/lib/prisma"
+import { authUser } from "~/shared/utils/abilities"
 
 interface Body {
     src: string
@@ -6,17 +8,36 @@ interface Body {
 }
 
 export default eventHandler(async(event) => {
-    const {src,alt} = await readBody<Body>(event)
-    const id = +getRouterParam(event, 'id')!
-    const rewriteImage =  await prisma.image.update({
-        where:{
-            id:+id
-        },
-        data:{
-            alt,
-            src
-        }
-    })
+    if ( authUser){ 
+        const { user } = useCurrentUser()
 
-    return rewriteImage
+        const {src,alt} = await readBody<Body>(event)
+        const id = +getRouterParam(event, 'id')!
+        const rewriteImage =  await prisma.image.update({
+            where:{
+                id:+id,
+                route_image:{
+                    route:{
+                        creater_id:user.yandexId
+                    }
+                },
+            // нужен ли or?
+
+                route_place_image:{
+                    route_place:{
+                        route:{
+                            creater_id:user.yandexId
+                        }
+                    }
+                }
+                
+            },
+            data:{
+                alt,
+                src
+            }
+        })
+
+        return rewriteImage
+    }
 })
