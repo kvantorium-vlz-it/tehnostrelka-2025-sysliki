98d495852e78b3ba104f3161bb36a3d773dd16b9
Sooouuus <ten0305alexey@gmail.com>
2025-02-20 19:01:12 +0300

    Добавил модуль

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-auth-utils', '@prisma/nuxt', 'nuxt-authorization'],
+  modules: ['nuxt-auth-utils', '@prisma/nuxt', 'nuxt-authorization', 'vue-yandex-maps/nuxt'],
   runtimeConfig: {
     oauth: {
       // provider in lowercase (github, google, etc.)
@@ -11,6 +11,8 @@ export default defineNuxtConfig({
         clientSecret:  process.env.YANDEX_CLIENT_SECRET
       }
     }
-  }
-
+  },
+  yandexMaps: {
+    apikey: 'b0fd084b-e4c3-478b-ac9b-3a9ad6094fb6',
+  },
 })
