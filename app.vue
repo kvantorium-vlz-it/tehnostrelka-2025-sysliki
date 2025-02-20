8fc425f79ca3a847ac99ae8652b2b142c2482177
Sooouuus <ten0305alexey@gmail.com>
2025-02-20 19:40:11 +0300

    Пример карты

--- app.vue
+++ app.vue
@@ -1,14 +1,47 @@
 <script setup lang="ts">
-  const { loggedIn, user, session, fetch, clear } = useUserSession()
+import type { LngLat } from '@yandex/ymaps3-types';
+const { loggedIn, user, session, fetch, clear } = useUserSession()
+  const points = ref<LngLat[]>([
+    [44.685846951952776, 48.78107863881559],
+    [44.787470487109026, 48.8121529405401],
+    [44.81012978886684, 48.772909916504],
+    [44.7311655554684, 48.74657938243983],
+  ])
+  const route = ref<LngLat[]>([])
 
-  
+  const joinedPointsToString = computed(() => points.value.map((point) => point.toReversed().join(',')).join('|'))
 
+  const fetchRoute = async () => {
+    const response = await $fetch(`https://api.geoapify.com/v1/routing?waypoints=${joinedPointsToString.value}&mode=walk&type=short&apiKey=c09299584a5e46fab02a6b984e1a1aa5`)
+
+    const coordinates = response.features[0].geometry.coordinates
+    const _points = coordinates.reduce((_c, __c) => [..._c, ...__c], []) as LngLat[]
+
+    route.value = _points
+  }
 </script>
 
 <template>
+  <RouteMap
+    v-model:points="points"
+    v-model:route="route"
+    @render-route="fetchRoute"
+  />
 
+  <hr>
+  Joined: {{ joinedPointsToString }}
 
- 
+  <hr>
+  Points: {{ points }}
+
+  <hr>
+
+  <button @click="() => points.push([44.763781217089495, 48.783801249140694])">
+    +
+  </button>
+
+  {{ route }}
+
   <div v-if="loggedIn">
     <h1>Welcome {{ user?.real_name}}!</h1>
     <p>Logged in since {{ session.loggedInAt  }}    </p>
